61ce3f69f6f4b1a9396fb1d3681ae9e679e04420
Leslie Grace Pelor <lppelor@student.apc.edu.ph>
2023-03-16 19:29:58 +0800

    afgaufa

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,8 +1,20 @@
 <!doctype html>
 <html>
+
+<?php
+$title = "Welcome to My Awesome Site";
+?>
+<!doctype html>
+<html>
+
 <head>
     <title>CodeIgniter Tutorial</title>
 </head>
+
 <body>
 
-    <h1><?= esc($title) ?></h1>
+    <h1><?php echo htmlspecialchars($title, ENT_QUOTES); ?></h1>
+
+</body>
+
+</html>
