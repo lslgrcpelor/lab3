b95b6243975b4f6b9854c630f798e7b517d65e79
Leslie Grace Pelor <lppelor@student.apc.edu.ph>
2023-03-16 19:33:57 +0800

    afuiafbas

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,18 +1,6 @@
 <!doctype html>
 <html>
 
-<?php
-$title = "Welcome to My Awesome Site";
-?>
-<!doctype html>
-<html>
-
-<!doctype html>
-<html>
-
-<!doctype html>
-<html>
-
 <head>
     <!-- No title tag -->
 </head>
